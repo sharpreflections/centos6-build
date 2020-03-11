3c58ae0e674350492c73633dc2b9a43fd5064b08
Dennis Brendel <dennis.brendel@sharpreflections.com>
2020-03-11 10:58:46 +0100

    Update build script
    - make use of fontconfig in order to have fonts
    - don't use some instruction sets to be compatible to more systems
    - disable not needed Qt modules

--- build_qt_qtwebkit_icc19.sh
+++ build_qt_qtwebkit_icc19.sh
@@ -97,7 +97,7 @@ done
 $runtime run $ENV $MOUNT --interactive --tty --name centos6-build-qt-icc19 sharpreflections/centos6-build bash -c "
 set -e
 
-yum -y install xz glibc-headers glibc-devel mesa-libGL-devel mesa-libEGL-devel openssl-devel
+yum -y install xz glibc-headers glibc-devel mesa-libGL-devel mesa-libEGL-devel openssl-devel fontconfig-devel
 
 cd /build
 echo \"Downloading Qt5 ${qt_version}:\"
@@ -120,17 +120,51 @@ mkdir build && cd build
                 -c++std c++11                \
                 -platform linux-icc-64       \
                 -ssl                         \
+                -fontconfig                  \
+                -system-freetype             \
                 -qt-zlib                     \
                 -qt-libjpeg                  \
                 -qt-libpng                   \
                 -nomake examples             \
                 -nomake tests                \
+                -no-sse4.1 -no-sse4.2        \
+                -no-avx -no-avx2 -no-avx512  \
                 -no-rpath                    \
                 -no-cups                     \
                 -no-iconv                    \
                 -no-dbus                     \
                 -no-gtk                      \
-                -no-glib
+                -no-glib                     \
+                -skip qt3d                   \
+                -skip qtandroidextras        \
+                -skip qtcanvas3d             \
+                -skip qtcharts               \
+                -skip qtconnectivity         \
+                -skip qtdatavis3d            \
+                -skip qtdeclarative          \
+                -skip qtgamepad              \
+                -skip qtlocation             \
+                -skip qtmacextras            \
+                -skip qtmultimedia           \
+                -skip qtnetworkauth          \
+                -skip qtpurchasing           \
+                -skip qtquickcontrols        \
+                -skip qtquickcontrols2       \
+                -skip qtscript               \
+                -skip qtsensors              \
+                -skip qtserialbus            \
+                -skip qtserialport           \
+                -skip qtspeech               \
+                -skip qtsvg                  \
+                -skip qttools                \
+                -skip qttranslations         \
+                -skip qtvirtualkeyboard      \
+                -skip qtwayland              \
+                -skip qtwebchannel           \
+                -skip qtwebengine            \
+                -skip qtwebsockets           \
+                -skip qtwinextras            \
+                -skip qtxmlpatterns
 
 make --jobs=$(nproc --all)
 make install
